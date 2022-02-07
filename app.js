9cff54aa4faa7049e4591a9a11322498ac2e9905
srijit-ops <srijitasengupta23@gmail.com>
2022-02-07 14:08:04 +0530

    Added my project

---
+++ app.js
@@ -0,0 +1,11 @@
+var options = {
+    strings: ['A Web-Developer!', 'A Programmer!','An Engineer!','A Student!','An Enthusiast!','An Artist!'],
+    loop: true,
+    typeSpeed: 100,
+    backSpeed: 100,
+    
+    cursorChar: "|"
+};
+
+var typed = new Typed('#element', options);
+AOS.init();
