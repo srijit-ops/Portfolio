167f7e81bbdb3175f10484ac9253b3d9d5bdb8d2
srijit-ops <srijitasengupta23@gmail.com>
2022-03-30 10:23:13 +0530

    nav film added

--- app.js
+++ app.js
@@ -9,3 +9,34 @@ var options = {
 
 var typed = new Typed('#element', options);
 AOS.init();
+
+let bar=document.getElementById("bar-icon")
+let mobileNav=document.getElementById("film")
+/*let navName=document.getElementById("navbar")*/
+let navOpen=false
+function navController(){
+    if(!navOpen){
+        navOpen=true
+        bar.classList.add("bar_open")
+        mobileNav.classList.add("open")
+        const links = document.querySelectorAll('.link')
+        setTimeout(()=>{
+            links.forEach(link=>{
+                link.style.opacity = '1'
+            })
+        },1000)
+        /*navName.classList.add("ph-my-nav")*/
+    }else{
+
+        const links = document.querySelectorAll('.link')
+        links.forEach(link=>{
+            link.style.opacity = '0'
+        })
+        setTimeout(()=>{
+            bar.classList.remove("bar_open")
+            mobileNav.classList.remove("open")
+            /*navName.classList.remove("ph-my-nav")*/
+            navOpen=false 
+        },300)
+    }
+}
