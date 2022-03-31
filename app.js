249986b2928eaa51d4ea1e90ce89f80960956f26
srijit-ops <srijitasengupta23@gmail.com>
2022-03-31 21:15:12 +0530

    Glitch Fixes and contact form added

--- app.js
+++ app.js
@@ -1,47 +1,50 @@
 var options = {
-    strings: ['A Web-Developer!', 'A Programmer!','An Engineer!','A Student!','An Enthusiast!','An Artist!'],
+    strings: ['A Web-Developer!', 'A Programmer!', 'An Engineer!', 'A Student!', 'An Enthusiast!', 'An Artist!'],
     loop: true,
     typeSpeed: 100,
     backSpeed: 100,
-    
-    cursorChar: "|"
+
+    cursorChar: "_"
 };
 
 var typed = new Typed('#element', options);
 AOS.init();
 
-let bar=document.getElementById("bar-icon")
-let mobileNav=document.getElementById("film")
-let navAnim=document.getElementById("navbar")
+let bar = document.getElementById("bar-icon")
+let mobileNav = document.getElementById("film")
+let navAnim = document.getElementById("navbar")
 
-let navOpen=false
-function navController(){
-    if(!navOpen){
-        navOpen=true
-        bar.classList.add("bar_open")
-        mobileNav.classList.add("open")
+let navOpen = false
+function navController() {
+    if (!navOpen) {
+        navOpen = true
+
         navAnim.classList.add("nav-color")
         const links = document.querySelectorAll('.link')
         setTimeout(()=>{
-            links.forEach(link=>{
+            bar.classList.add("bar_open")
+            mobileNav.classList.add("open")
+        },100)
+        setTimeout(() => {
+            links.forEach(link => {
                 link.style.opacity = '1'
             })
-        },1000)
-       
-    }else{
+        }, 1000)
+
+    } else {
 
         const links = document.querySelectorAll('.link')
-        links.forEach(link=>{
+        links.forEach(link => {
             link.style.opacity = '0'
         })
-        setTimeout(()=>{
+        setTimeout(() => {
             bar.classList.remove("bar_open")
             mobileNav.classList.remove("open")
-            
-            navOpen=false 
-        },300)
-        setTimeout(()=>{
-            navAnim.classList.remove("nav-color")
-        },820)
+            setTimeout(()=>{
+                navAnim.classList.remove("nav-color")
+            },1000)
+
+            navOpen = false
+        }, 300)
     }
 }
